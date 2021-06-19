e9c01374c845d7dc24eb9f2e7ecb430daa5ae4b9
Borislav Slavov <bobi_slavov@yahoo.com>
2021-06-19 13:46:41 +0300

    Format with Prettier

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -1,32 +1,34 @@
-import CartItem from '../CartItem/CartItem';
+import CartItem from "../CartItem/CartItem";
 // Styles
-import { Wrapper } from './Cart.styles';
+import { Wrapper } from "./Cart.styles";
 // Types
-import { CartItemType } from '../App';
+import { CartItemType } from "../App";
 
 type Props = {
-    cartItems: CartItemType[];
-    addToCart: (cartItem: CartItemType) => void;
-    removeFromCart: (id: number) => void;
+  cartItems: CartItemType[];
+  addToCart: (cartItem: CartItemType) => void;
+  removeFromCart: (id: number) => void;
 };
 
 const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
-    const calculateTotal = (items: CartItemType[]) =>
-    items.reduce((acc:number, item) => acc + (item.amount * item.price), 0);
- return (
- <Wrapper>
-    <h2>Your Shopping Cart</h2>
-    {cartItems.length === 0 ? <p>No items in cart</p> : null}
-    {cartItems.map(item => (
+  const calculateTotal = (items: CartItemType[]) =>
+    items.reduce((acc: number, item) => acc + item.amount * item.price, 0);
+
+  return (
+    <Wrapper>
+      <h2>Your Shopping Cart</h2>
+      {cartItems.length === 0 ? <p>No items in cart</p> : null}
+      {cartItems.map((item) => (
         <CartItem
-            key={item.id}
-            item={item}
-            addToCart={addToCart}
-            removeFromCart={removeFromCart}
-         />
-    ))}
-    <h2>Total: ${calculateTotal(cartItems)}</h2>
-</Wrapper>
- )};
+          key={item.id}
+          item={item}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+        />
+      ))}
+      <h2>Total: ${calculateTotal(cartItems)}</h2>
+    </Wrapper>
+  );
+};
 
 export default Cart;
